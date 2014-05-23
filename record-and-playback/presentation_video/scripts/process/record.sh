416aafd29287bc719be07a8300240dbf8b4e08ad
Felipe <felipefsch@gmail.com>
2014-05-23 17:25:43 +0000

    Adding firefox permission solution to branch

--- record-and-playback/presentation_video/scripts/process/record.sh
+++ record-and-playback/presentation_video/scripts/process/record.sh
@@ -1,7 +1,9 @@
 # @author Felipe Mathias Schmidt, PRAV - MConf (UFRGS)
 #
-# This script creates a virtual display, fires an instance of firefox inside it with a given video link. Firefox version
-# used is 27.0.
+# This script creates a virtual display, fires an instance of firefox inside it with a video link. Firefox version used
+# is 27.0; the web-browser must be previously configurated, creating as much profiles as desired to your recording pool
+# (number of simultaneous recording). Profile names must be the same as the virtual display ID (e.g: 99).
+# By the end of this process, all used programs are closed/terminated.
 #
 # Parameters
 # $1 - first parameter must be the DISPLAY_ID, an unique ID that identifies the virtual display
@@ -20,23 +22,31 @@ TIME=$2
 WEB_LINK=$3
 OUTPUT_PATH=$4
 
+export DISPLAY=:$DISPLAY_ID
+
 # Create new Xvfb display
 Xvfb :$DISPLAY_ID -nocursor -screen 0 $DISPLAY_SETTING &
-XVFB=$!
+XVFB_PID=$!
 
-# Open firefox on new display -p $DISPLAY_ID 
-firefox -profile /tmp/presentation_video-firefox-profile/ -safe-mode --display :$DISPLAY_ID -width $FIREFOX_WIDTH -height $FIREFOX_HEIGHT -new-window $WEB_LINK &
-FIREFOX=$!
+FIREFOX_HOME=/tmp/firefox_presentation_video
+FIREFOX_PROFILE=$FIREFOX_HOME/new_profile/
 
-# Xvfb :96 -screen 0 1400x768x24 &
-# firefox -profile /tmp/presentation_video-firefox-profile/ -p 96 -safe-mode --display :96 -new-window www.terra.com.br &
-# firefox -p 96 -safe-mode --display :96 -new-window www.terra.com.br &
-# recordmydesktop --display :96 --no-sound
-# DISPLAY=:96 xdotool key Return
+rm -rf $FIREFOX_HOME
+mkdir -p $FIREFOX_HOME
+mkdir -p $FIREFOX_PROFILE
+
+# Open firefox on new display
+HOME=$FIREFOX_HOME firefox -profile $FIREFOX_PROFILE -safe-mode -width $FIREFOX_WIDTH -height $FIREFOX_HEIGHT -new-window $WEB_LINK &
+FIREFOX_PID=$!
 
 # Press enter to skip safemode
 sleep $SAFEMODE_WAIT
-DISPLAY=:$DISPLAY_ID xdotool key Return
+xdotool key Return
+
+# Click to close the Mozilla Foundation message
+sleep $SAFEMODE_WAIT
+xdotool mousemove $(($FIREFOX_WIDTH - 14)) 100
+xdotool click 1
 
 #Get meeting id from web link
 MEETING_ID=$(echo $WEB_LINK | cut -d '=' -f2)
@@ -44,27 +54,25 @@ MEETING_ID=$(echo $WEB_LINK | cut -d '=' -f2)
 # Move mouse to start playing the video
 sleep $FIREFOX_LOAD_WAIT
 
-DISPLAY=:$DISPLAY_ID xdotool mousemove $PLAY_BUTTON_X_POSITION $PLAY_BUTTON_Y_POSITION
-DISPLAY=:$DISPLAY_ID xdotool click 1
+xdotool mousemove $PLAY_BUTTON_X_POSITION $PLAY_BUTTON_Y_POSITION
+xdotool click 1
 
 # Start recording
-recordmydesktop --full-shots --display :$DISPLAY_ID --no-cursor --no-sound --width $RECORD_WINDOW_WIDTH --height $RECORD_WINDOW_HEIGHT -x $RECORD_WINDOW_X_OFFSET -y $RECORD_WINDOW_Y_OFFSET -o $OUTPUT_PATH &
-RECORD=$!
-
-DISPLAY=:$DISPLAY_ID xdotool key Return
+recordmydesktop --full-shots --no-cursor --no-sound --width $RECORD_WINDOW_WIDTH --height $RECORD_WINDOW_HEIGHT -x $RECORD_WINDOW_X_OFFSET -y $RECORD_WINDOW_Y_OFFSET -o $OUTPUT_PATH &
+RECORD_PID=$!
 
 # Sleep used to keep recording for the defined time
 sleep $TIME
 
 # Finish recording and start storing the video
-kill -s 15 $RECORD
+kill -s 15 $RECORD_PID
 
-kill -s 15 $FIREFOX
+kill -s 15 $FIREFOX_PID
 
 # Waiting to store the recorded video
-wait $RECORD
+wait $RECORD_PID
 
-kill -s 15 $XVFB
+kill -s 15 $XVFB_PID
 
 echo "Recording at display $DISPLAY_ID during $TIME seconds terminated."
 
