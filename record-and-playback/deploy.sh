a926e3e9fc71dad709866b43bd2bd30310fbac10
Felipe Schmidt <felipefsch@gmail.com>
2014-09-26 18:33:09 +0200

    Cherry-pick deploy.sh
    Conflicts:
    	record-and-playback/deploy.sh

--- record-and-playback/deploy.sh
+++ record-and-playback/deploy.sh
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with BigBlueButton; if not, see <http://www.gnu.org/licenses/>.
 #
-
 set -xe
 
 # sudo service mconf-presentation-recorder stop
@@ -26,26 +25,36 @@ sudo rm -rf /usr/local/bigbluebutton/core/lib
 sudo cp -r core/lib /usr/local/bigbluebutton/core/
 sudo rm -rf /usr/local/bigbluebutton/core/scripts
 sudo cp -r core/scripts /usr/local/bigbluebutton/core/
+
 sudo rm -f /etc/init.d/bbb-record-core
 sudo cp core/bbb-record-core /etc/init.d/bbb-record-core
 sudo chmod 0755 /etc/init.d/bbb-record-core
-sudo rm -rf /var/bigbluebutton/playback/*
 
+sudo rm -rf /var/bigbluebutton/playback/*
 function deploy_format() {
-	local formats=$1
-	for format in $formats
-	do
-		playback_dir="$format/playback/$format"
-		scripts_dir="$format/scripts"
-		if [ -d $playback_dir ]; then sudo cp -r $playback_dir /var/bigbluebutton/playback/; fi
-		if [ -d $scripts_dir ]; then sudo cp -r $scripts_dir/* /usr/local/bigbluebutton/core/scripts/; fi
-		sudo mkdir -p /var/log/bigbluebutton/$format /var/bigbluebutton/published/$format /var/bigbluebutton/recording/publish/$format
-	done
+  local formats=$1
+  for format in $formats
+  do
+    playback_dir="$format/playback/$format"
+    scripts_dir="$format/scripts"
+    if [ -d $playback_dir ]; then sudo cp -r $playback_dir /var/bigbluebutton/playback/; fi
+    if [ -d $scripts_dir ]; then sudo cp -r $scripts_dir/* /usr/local/bigbluebutton/core/scripts/; fi
+    sudo mkdir -p /var/log/bigbluebutton/$format /var/bigbluebutton/published/$format /var/bigbluebutton/recording/publish/$format
+  done
 }
 
-deploy_format "presentation"
-deploy_format "presentation_video"
-sudo mv /usr/local/bigbluebutton/core/scripts/mconf-presentation-recorder.initd /etc/init.d/mconf-presentation-recorder
+RECORDING_SERVER=false
+
+if $RECORDING_SERVER ; then
+  deploy_format "presentation"
+  deploy_format "presentation_video"
+  sudo mv /usr/local/bigbluebutton/core/scripts/mconf-presentation-recorder.initd /etc/init.d/mconf-presentation-recorder
+  deploy_format "mconf_decrypter"
+  sudo mv /usr/local/bigbluebutton/core/scripts/mconf-recording-decrypter.initd /etc/init.d/mconf-recording-decrypter
+  sudo mv /usr/local/bigbluebutton/core/scripts/mconf-recording-decrypter.monit /etc/monit/conf.d/mconf-recording-decrypter
+else
+  deploy_format "mconf_encrypted"
+fi
 
 sudo mkdir -p /var/bigbluebutton/playback/
 sudo mkdir -p /var/bigbluebutton/recording/raw/
@@ -56,11 +65,12 @@ sudo mkdir -p /var/bigbluebutton/recording/status/archived/
 sudo mkdir -p /var/bigbluebutton/recording/status/processed/
 sudo mkdir -p /var/bigbluebutton/recording/status/presentation_video/
 sudo mkdir -p /var/bigbluebutton/recording/status/sanity/
-
 sudo mv /usr/local/bigbluebutton/core/scripts/*.nginx /etc/bigbluebutton/nginx/
 sudo chown -R tomcat7:tomcat7 /var/bigbluebutton/ /var/log/bigbluebutton/
 sudo chown -R red5:red5 /var/bigbluebutton/deskshare/
 sudo chown -R freeswitch:daemon /var/bigbluebutton/meetings/
+
+pushd . > /dev/null
 
 cd /usr/local/bigbluebutton/core/
 sudo bundle install
