aa632a780ccdd3ca9fb29ef4a3bea66fcb4c103a
Felipe Schmidt <felipefsch@gmail.com>
2014-12-22 15:58:09 +0100

    Scaling deskshare video, adding white padding to cover video

--- record-and-playback/presentation_video/merge.sh
+++ record-and-playback/presentation_video/merge.sh
@@ -1,3 +1,9 @@
 DESKSHARE_DELAY=7
 
-ffmpeg -i video.webm -i deskshare.webm -filter_complex "[0:v] setpts=PTS-STARTPTS, scale=1222x604 [base]; [1:v] setpts=PTS-STARTPTS+$DESKSHARE_DELAY/TB, scale=810x604 [deskshare]; [base][deskshare] overlay=w:x=0:y=0" -c:a libvorbis -b:a 48K -f webm myout.webm
+DESKSHARE_WIDTH=1222 
+DESKSHARE_HEIGHT=604
+
+MAX_WIDTH=810
+MAX_HEIGHT=604
+
+ffmpeg -i video.webm -i deskshare.webm -filter_complex "[0:v] setpts=PTS-STARTPTS [presentation_video]; [1:v] setpts=PTS-STARTPTS+$DESKSHARE_DELAY/TB, scale=810:-2, pad=width=$MAX_WIDTH:height=$MAX_HEIGHT:x=0:y=0:color=white [deskshare]; [presentation_video][deskshare] overlay=eof_action=pass:x=0:y=0" -c:a libvorbis -b:a 48K -f webm myout.webm
